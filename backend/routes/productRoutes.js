87872d27a34a7ba7bb9d8d2c21a1867dd6380cc7
Angelo Bihis <anjo.bihis@gmail.com>
2021-01-15 19:36:33 +0800

    added product list edit,remove

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,12 @@ import express from 'express'
 const router = express.Router()
 import {
   getProductById,
-  getProducts
+  getProducts,
+  deleteProduct
 } from '../controllers/productController.js'
+import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts)
-
-router.route('/:id').get(getProductById)
+router.route('/:id').get(getProductById).delete(protect, isAdmin, deleteProduct)
 
 export default router
