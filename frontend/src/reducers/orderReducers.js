fb27bbfe8f733f22d9f376d04123140a148c631e
Angelo Bihis <anjo.bihis@gmail.com>
2021-01-15 10:36:39 +0800

    added payment for paypal redux

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -4,7 +4,11 @@ import {
   ORDER_CREATE_REQUEST,
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
-  ORDER_DETAILS_FAIL
+  ORDER_DETAILS_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_FAIL,
+  ORDER_PAY_RESET
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -36,12 +40,12 @@ export const orderDetailsReducer = (
   switch (action.type) {
     case ORDER_DETAILS_REQUEST:
       return {
+        ...state,
         loading: true
       }
     case ORDER_DETAILS_SUCCESS:
       return {
         loading: false,
-        success: true,
         order: action.payload
       }
     case ORDER_DETAILS_FAIL:
@@ -49,6 +53,29 @@ export const orderDetailsReducer = (
         loading: false,
         error: action.payload
       }
+    default:
+      return state
+  }
+}
+
+export const orderPayReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ORDER_PAY_REQUEST:
+      return {
+        loading: true
+      }
+    case ORDER_PAY_SUCCESS:
+      return {
+        loading: false,
+        success: true
+      }
+    case ORDER_PAY_FAIL:
+      return {
+        loading: false,
+        error: action.payload
+      }
+    case ORDER_PAY_RESET:
+      return {}
     default:
       return state
   }
