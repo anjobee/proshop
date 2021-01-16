65afbcd2091cab88491a415491ac51c25363f674
Angelo Bihis <anjo.bihis@gmail.com>
2021-01-16 11:15:52 +0800

    added product review functionality in the backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,11 +5,13 @@ import {
   getProducts,
   deleteProduct,
   createProduct,
-  updateProduct
+  updateProduct,
+  createProductReview
 } from '../controllers/productController.js'
 import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, isAdmin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
 router
   .route('/:id')
   .get(getProductById)
