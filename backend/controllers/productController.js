28994f8a612fc2461e4eab1954e36ba6cc4890b5
Angelo Bihis <anjo.bihis@gmail.com>
2021-01-16 13:11:20 +0800

    implemented pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,9 +5,26 @@ import Product from '../models/productModel.js'
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({})
+  //req.query -- query is used to get strings after "?" on params i.e. (/api/products?foo=bar)
 
-  res.json(products)
+  const pageSize = 10
+  const page = Number(req.query.pageNumber) || 1
+
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: 'i'
+        }
+      }
+    : {}
+
+  const count = await Product.countDocuments({ ...keyword })
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1))
+
+  res.json({ products, page, pages: Math.ceil(count / pageSize) })
 })
 
 // @desc    Fetch single product
