8cc64543e2f10c130e43b585e76429557c47974d
Angelo Bihis <anjo.bihis@gmail.com>
2021-01-29 22:33:33 +0800

    added new products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,7 +7,7 @@ import Product from '../models/productModel.js'
 const getProducts = asyncHandler(async (req, res) => {
   //req.query -- query is used to get strings after "?" on params i.e. (/api/products?foo=bar)
 
-  const pageSize = 10
+  const pageSize = 8
   const page = Number(req.query.pageNumber) || 1
 
   const keyword = req.query.keyword
